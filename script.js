76b13a6c9c5f476fffcd5112a4ef1f05ce98abea
alibek <bek29a@gmail.com>
2024-01-14 11:20:39 +0600

    урок 19

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ genres
 
 P.S. Функции вызывать не обязательно*/
 
+
+/*
 'use strict';
 
 let numberOfFilms;
@@ -82,3 +84,30 @@ function writeYourGenres(){
 
 writeYourGenres();
 // Код возьмите из предыдущего домашнего задания
+
+*/
+
+"use strict"
+
+function first() {
+    // Do something
+    setTimeout(function(){
+        console.log(1);
+    }, 500);
+}
+
+function second(){
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
+}
+
+learnJS('Javascript', function() {
+    console.log("Я прошел этот урок!");
+}); 
